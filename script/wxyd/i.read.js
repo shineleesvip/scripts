de31d595cf3056417e55c35ad39288ee5a847ab6
shine <shinelee_vip@163.com>
2022-07-11 15:28:32 +0800

    1

--- script/wxyd/i.read.js
+++ script/wxyd/i.read.js
@@ -5,16 +5,27 @@
 hostname = i.weread.qq.com
 */
 
-var body = $response;
+// var body = $response;
+// var url = $request.url;
+// const path = "/pay/memberCardSummary";
+// let obj = JSON.parse(body);
+// if (url.indexOf(path) != -1) {
+// 	obj.expiredTime = 2998743249;
+// 	obj.expired = 0;
+// 	obj.remainTime = 189302400;
+// 	obj.isPaying = 1;
+	
+// 	body = JSON.stringify(obj);
+// }
+// $done({body});
+var body = $response.body;
 var url = $request.url;
 const path = "/pay/memberCardSummary";
 let obj = JSON.parse(body);
 if (url.indexOf(path) != -1) {
-	obj.expiredTime = 2998743249;
-	obj.expired = 0;
-	obj.remainTime = 189302400;
-	obj.isPaying = 1;
-	
+    obj["expired"] = 0;
+	obj["expiredTime"] = 1591804799;
+	obj["remainTime"] = 86313600;
 	body = JSON.stringify(obj);
-}
+ }
 $done({body});
