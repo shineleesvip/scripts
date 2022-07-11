805f5189535ad0885cd9b1eab35905b5eb5bf04d
shine <shinelee_vip@163.com>
2022-07-11 15:37:24 +0800

    fix

--- script/wxyd/i.read.js
+++ script/wxyd/i.read.js
@@ -5,27 +5,15 @@
 hostname = i.weread.qq.com
 */
 
-// var body = $response;
-// var url = $request.url;
-// const path = "/pay/memberCardSummary";
-// let obj = JSON.parse(body);
-// if (url.indexOf(path) != -1) {
-// 	obj.expiredTime = 2998743249;
-// 	obj.expired = 0;
-// 	obj.remainTime = 189302400;
-// 	obj.isPaying = 1;
-	
-// 	body = JSON.stringify(obj);
-// }
-// $done({body});
 var body = $response.body;
 var url = $request.url;
 const path = "/pay/memberCardSummary";
 let obj = JSON.parse(body);
 if (url.indexOf(path) != -1) {
     obj["expired"] = 0;
-	obj["expiredTime"] = 1591804799;
-	obj["remainTime"] = 86313600;
+	obj["expiredTime"] = 2998743249;
+	obj["remainTime"] = 189302400;
+	obj["isPaying"] = 1;
 	body = JSON.stringify(obj);
  }
 $done({body});
