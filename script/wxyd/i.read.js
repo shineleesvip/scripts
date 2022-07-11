6844d84bf61d892ff4e1074f557a48d9aca076f2
shine <shinelee_vip@163.com>
2022-07-11 14:44:06 +0800

    fist

---
+++ script/wxyd/i.read.js
@@ -0,0 +1,20 @@
+/*微信阅读
+[Script]
+^https?:\/\/i\.weread\.qq\.com\/pay\/memberCardSummary
+[MITM]
+hostname = i.weread.qq.com
+*/
+
+var body = $respone;
+var url = $request.url;
+const path = "/pay/memberCardSummary";
+let obj = JSON.parse(body);
+if (url.indexOf(path) != -1) {
+	obj.expiredTime = 2998743249;
+	obj.expired = 0;
+	obj.remainTime = 189302400;
+	obj.isPaying = 1;
+	
+	body = JSON.stringify(obj);
+}
+$done({body});
